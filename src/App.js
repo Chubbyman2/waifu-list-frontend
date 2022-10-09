4dddfa375a1b3cd396180d8fbb386dba3d87e514
Charles Yuan <charlesyuan59@gmail.com>
2022-10-09 16:34:22 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+
+
+function App() {
+    const [waifu, setWaifu] = useState("");
+    const [waifus, setWaifus] = useState([]);
+
+    const getAllWaifus = () => {
+        fetch("http://waifu-list.eba-wprwgyza.us-east-1.elasticbeanstalk.com/api/all")
+        .then(res => res.json())
+        .then(data => {
+            // Make sure to sort the array of waifus by rank
+            setWaifus(data.sort(function(a, b){
+                return a.rank - b.rank;
+            }));
+        });
+    }
+
+    const addWaifu = () => {
+        fetch("http://waifu-list.eba-wprwgyza.us-east-1.elasticbeanstalk.com/api", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name: "Makise Kurisu",
+                anime: "Steins;Gate",
+                rank: 2,
+                description: "The assistant",
+                image: "makise_kurisu.png"
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        });
+    }
+
+    const getWaifu = () => {
+        Axios.get("http://waifu-list.eba-wprwgyza.us-east-1.elasticbeanstalk.com/api", {
+            params: {
+                name: "Lynn Wiles"
+            }
+        })
+        .then(res => res.data)
+        .then(data => {
+            console.log(data);
+            setWaifu(data);
+        });
+    }
+
+    // Run this code once when the component is mounted
+    useEffect(() => {
+        getWaifu();
+        getAllWaifus();
+    }, []);
+
+    return (
+        <table className="waifu-table">
+        <thead>
+            <tr>
+                <th><center>Rank</center></th>
+                <th><center>Name</center></th>
+                <th><center>Anime</center></th>
+                <th><center>Image</center></th>
+            </tr>
+        </thead>
+        
+        {waifus.map(function(waifu, i) {
+            return (
+                <tr key={i}>
+                    <td width="10%">{waifu.rank}</td>
+                    <td width="30%">{waifu.name}</td>
+                    <td width="30%">{waifu.anime}</td>
+                    <td width="30%"><img src={`${process.env.REACT_APP_SUPABASE_BUCKET}${waifu.image}`} width="100%"></img></td>
+                </tr>
+            );
+        })}
+        </table>
+    );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom"; // For rendering the element in the actual DOM
+import "bootstrap/dist/css/bootstrap.css";
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />,
+);
