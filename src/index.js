49843703ead041d205fae8e2c02088c70cf09fdf
Charles Yuan <charlesyuan59@gmail.com>
2022-10-09 16:54:01 -0400

    Removed bootstrap.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom"; // For rendering the element in the actual DOM
-import "bootstrap/dist/css/bootstrap.css";
 import App from './App';
 
 
